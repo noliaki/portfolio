a8837c7e7db9eb2ce9495898e5d4ab363d0c1e61
noliaki <noliaki@gmail.com>
2019-04-07 00:17:41 +0900

    :+1: fix bg canvas

--- src/components/Canvas.vue
+++ src/components/Canvas.vue
@@ -7,7 +7,7 @@
       | loading next background image...
 </template>
 <script>
-/* global PIXI TweenLite Power0 Power1 */
+/* global PIXI TweenLite Power1 */
 
 import _debounce from 'lodash/debounce'
 import { mapGetters } from 'vuex'
@@ -17,12 +17,14 @@ export default {
     return {
       app: undefined,
       container: undefined,
-      images: [],
+      meshes: new Map(),
       currentImageIndex: 0,
       isAnimating: false,
       displacementSprite: undefined,
       isLoadingNextImage: false,
-      distance: undefined
+      distance: 0,
+      verticesMap: new Map(),
+      displacementFilterMap: new Map()
     }
   },
   computed: {
@@ -31,7 +33,7 @@ export default {
   watch: {
     $route() {
       if (!this.isAnimating && !this.isLoadingNextImage) {
-        this.outImage(this.currentImageIndex)
+        this.inImage(this.currentImageIndex)
       }
     }
   },
@@ -72,39 +74,73 @@ export default {
   },
   methods: {
     async inImage(imageIndex, isFirst = false) {
-      if (!this.images[imageIndex]) {
+      if (!this.isAnimating) {
+        this.isAnimating = true
+      }
+
+      if (!this.meshes.has(imageIndex)) {
         this.isLoadingNextImage = true
-        this.images[imageIndex] = await this.loadAndCreate(imageIndex)
+        this.meshes.set(imageIndex, await this.loadAndCreate(imageIndex))
         this.isLoadingNextImage = false
       }
 
-      this.initializeImage(this.images[imageIndex])
-      this.fitToWindow(this.images[imageIndex].mesh)
+      this.fitToWindow(imageIndex)
 
-      if (isFirst) {
-        if (!this.isAnimating) {
-          this.isAnimating = true
-        }
-
-        this.images[imageIndex].mesh.alpha = 0
-
-        TweenLite.to(this.images[imageIndex].mesh, 0.5, {
-          alpha: 1,
-          onComplete: () => {
-            this.isAnimating = false
-          },
-          ease: Power0.easeNone
-        })
-      } else {
-        this.images[imageIndex].mesh.alpha = 1
+      const mesh = this.meshes.get(imageIndex)
+      const vertices = this.verticesMap.get(mesh)
+      const displacementFilter = this.displacementFilterMap.get(mesh)
+      const randRatio = vertices.map(item => Math.random() * -2 + 1)
+      const obj = {
+        val: 1,
+        scaleX: Math.random() * 1000 + 1000,
+        scaleY: Math.random() * 1000 + 1000
       }
 
-      this.container.addChildAt(this.images[imageIndex].mesh, 0)
+      mesh.alpha = 0
+      displacementFilter.scale.x = obj.scaleX
+      displacementFilter.scale.y = obj.scaleY
+      for (let i = 0, len = vertices.length; i < len; i++) {
+        mesh.vertices[i] = vertices[i] + randRatio[i] * this.distance
+      }
+
+      this.container.addChild(mesh)
+      mesh.visible = true
+
+      TweenLite.to(obj, 1, {
+        val: 0,
+        scaleX: 0,
+        scaleY: 0,
+        onUpdate: () => {
+          mesh.alpha = 1 - obj.val
+
+          displacementFilter.scale.x = obj.val * obj.scaleX
+          displacementFilter.scale.y = obj.val * obj.scaleY
+
+          for (let i = 0, len = mesh.vertices.length; i < len; i++) {
+            mesh.vertices[i] =
+              vertices[i] + randRatio[i] * this.distance * obj.val
+          }
+        },
+        onComplete: () => {
+          this.isAnimating = false
+          this.meshes.forEach((mesh, key, map) => {
+            if (key !== imageIndex) {
+              mesh.visible = false
+            }
+          })
+          this.currentImageIndex =
+            (this.currentImageIndex + 1) % this.entries.length
+        },
+        ease: Power1.easeInOut
+      })
 
       const nextImageIndex = (this.currentImageIndex + 1) % this.entries.length
-      if (!this.images[nextImageIndex]) {
+      if (!this.meshes.has(nextImageIndex)) {
         this.isLoadingNextImage = true
-        this.images[nextImageIndex] = await this.loadAndCreate(nextImageIndex)
+        this.meshes.set(
+          nextImageIndex,
+          await this.loadAndCreate(nextImageIndex)
+        )
         this.isLoadingNextImage = false
       }
     },
@@ -115,62 +151,23 @@ export default {
 
       return this.createMesh(texure)
     },
-    outImage(imageIndex) {
-      this.isAnimating = true
-
-      const obj = {
-        alpha: 1,
-        scaleX: 0,
-        scaleY: 0,
-        translate: 0
-      }
-
-      const displacementFilter = this.images[imageIndex].displacementFilter
-      const mesh = this.images[imageIndex].mesh
-      const origVertices = this.images[imageIndex].originalVertices
-      const ratio = origVertices.map(item => Math.random())
-
-      this.currentImageIndex =
-        (this.currentImageIndex + 1) % this.entries.length
-      this.inImage(this.currentImageIndex)
-
-      TweenLite.to(obj, 1, {
-        alpha: 0,
-        scaleX: Math.random() * 1000 + 1000,
-        scaleY: Math.random() * 1000 + 1000,
-        translate: this.distance,
-        onUpdate: () => {
-          mesh.alpha = obj.alpha
-          displacementFilter.scale.x = obj.scaleX
-          displacementFilter.scale.y = obj.scaleY
-
-          for (let i = 0, len = mesh.vertices.length; i < len; i++) {
-            mesh.vertices[i] = origVertices[i] + ratio[i] * obj.translate
-          }
-        },
-        onComplete: () => {
-          this.isAnimating = false
-        },
-        ease: Power1.easeInOut
-      })
-    },
     createMesh(texture) {
       const mesh = new PIXI.mesh.Plane(texture, 10, 10)
-      const originalVertices = mesh.vertices.slice()
       const displacementFilter = new PIXI.filters.DisplacementFilter(
         this.displacementSprite,
         0
       )
 
-      mesh.filters = [displacementFilter]
+      this.verticesMap.set(mesh, mesh.vertices.slice())
+      this.displacementFilterMap.set(mesh, displacementFilter)
 
-      return {
-        mesh,
-        originalVertices,
-        displacementFilter
-      }
+      mesh.filters = [displacementFilter]
+      // mesh.anchor.set(0.5)
+
+      return mesh
     },
-    fitToWindow(mesh) {
+    fitToWindow(imageIndex) {
+      const mesh = this.meshes.get(imageIndex)
       const texture = mesh._texture
       mesh.x = 0
       mesh.y = 0
@@ -185,18 +182,9 @@ export default {
         mesh.y = (window.innerHeight - mesh.height) / 2
       } else {
         const ratio = window.innerHeight / texture.height
-        mesh.height = window.innerHeight
         mesh.width = texture.width * ratio
+        mesh.height = window.innerHeight
         mesh.x = (window.innerWidth - mesh.width) / 2
-        mesh.y = 0
-      }
-    },
-    initializeImage(imageObj) {
-      imageObj.displacementFilter.scale.x = 0
-      imageObj.displacementFilter.scale.y = 0
-
-      for (let i = 0, len = imageObj.mesh.vertices.length; i < len; i++) {
-        imageObj.mesh.vertices[i] = imageObj.originalVertices[i]
       }
     },
     loadImageAsTexture(imagePath) {
@@ -216,8 +204,8 @@ export default {
       this.$refs.canvas.style.display = 'none'
       this.app.renderer.resize(window.innerWidth, window.innerHeight)
 
-      if (this.images[this.currentImageIndex]) {
-        this.fitToWindow(this.images[this.currentImageIndex].mesh)
+      if (this.meshes.has(this.currentImageIndex)) {
+        this.fitToWindow(this.currentImageIndex)
       }
 
       this.displacementSprite.width = window.innerWidth
